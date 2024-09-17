a544ced146d351a0981262513d86e332eb703754
RizaAlraihany <azieralraihany@gmail.com>
2024-09-17 15:46:23 +0700

    first commit

---
+++ scripct.js
@@ -0,0 +1,14 @@
+// toggle active
+const navbarNav = document.querySelector('.navbar-nav')
+// menu diklik
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+}
+// menghilangkan nab
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function (e) {
+    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+})
